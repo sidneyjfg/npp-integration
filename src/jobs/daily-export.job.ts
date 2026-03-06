a309fae4a69da4446760c00d791dd757f89fb87a
sidneyjfg <sidneyjunio191@gmail.com>
2026-03-06 08:37:08 -0300

    Feat: Adicionando env para cron

--- src/jobs/daily-export.job.ts
+++ src/jobs/daily-export.job.ts
@@ -1,11 +1,14 @@
 import cron from "node-cron";
 import { ExportService } from "../modules/export/export.service";
+import dotenv from "dotenv";
 
+dotenv.config();
 export function startDailyExportJob() {
+  const CRON = process.env.CRON || "0 2 * * *"; // padrão: todo dia às 02:00
   const service = new ExportService();
 
   // todo dia às 02:00
-  cron.schedule("0 2 * * *", async () => {
+  cron.schedule(CRON, async () => {
     console.log("Iniciando exportação NAPP...");
 
     try {
